ed67932f9a0233bce012da664a948575044e9fbe
Michael Iwanek <miwanek11199@gmail.com>
2023-06-02 15:53:37 -0700

    add carousel

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
+import ImageGallery from 'react-image-gallery';
+import Shop_Image_1 from '../images/shop_1.png'
+import Shop_Image_2 from '../images/shop_2.png'
+import Shop_Image_3 from '../images/shop_3.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const HomePage = () => {
+
+
   return (
 <>
         <nav>
@@ -35,7 +43,7 @@ const HomePage = () => {
         
         <section id="projects" style={{margin:"0px"}}>
         	<h1 style={{margin:"0px"}}> Projects </h1>
-          <p style={{height:"400px", margin:"0px"}}>
+          <p style={{height:"auto", marginBottom:"150px"}}>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Vero, nam! Iure officia aut esse tempore accusantium
                 explicabo? Corporis deleniti ipsa fuga quas aut neque
@@ -45,7 +53,28 @@ const HomePage = () => {
                 necessitatibus consectetur, labore vero et ipsum.
                 Officiis, ea vero. Praesentium, et. Enim, nostrum illo.
             </p>
+
+          <h2 style={{marginBottom:"30px"}}>
+            Ski & Rock Climbing E-Commerce Store
+          </h2>   
+          <div class="project_card">     
+              <Carousel width="50%" autoPlay="true">
+                    <div>
+                        <img alt="shop_1" src={Shop_Image_1} />
+                        {/* <p className="legend">Legend 1</p> */}
+                    </div>
+                    <div>
+                        <img alt="shop_2" src={Shop_Image_2} />
+                        {/* <p className="legend">Legend 2</p> */}
+                    </div>
+                    <div>
+                        <img alt="shop_3"src={Shop_Image_3} />
+                        {/* <p className="legend">Legend 3</p> */}
+                    </div>
+              </Carousel>
+          </div>
         </section>
+
         
         <section id="profile">
         	<h1 style={{margin:"0px", paddingTop:"40px"}}> Profile </h1>
