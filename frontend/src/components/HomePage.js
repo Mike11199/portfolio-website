eda2c7813d573ca317df0ac673d3929b7d811ce0
Michael Iwanek <miwanek11199@gmail.com>
2023-06-03 23:00:48 -0700

    added section for smallsh - not github link yet as class still in progress

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -388,6 +388,79 @@ const HomePage = () => {
 
         </section>   
 
+
+
+
+
+
+
+
+
+
+           {/* SECTION FOR PROJECT #5 */}
+           <section id="projects3"  style={{margin:"0px", height:"800px"}}>       
+              <h2 style={{margin:"0px", marginTop:"0px", paddingTop:"40px", paddingBottom:"40px"}}>          
+              Small Shell (smallsh) - C Unix Shell
+              </h2>   
+              <div style={{margin:"0px", display:"flex", flexDirection: "row"}}>
+              <div style={{margin:"0px", display:"flex", flexDirection: "column", width:"100%"}}>
+              <div class="project_card" style={{marginBottom:"50px"}}>     
+            
+              </div>
+          
+              <div className='player-wrapper' style={{paddingRight:"70px", paddingLeft:"120px", paddingBottom:"70px"}} >
+                <ReactPlayer   
+                  width={800}
+                  height={400}
+                  playing
+                  url="https://www.youtube.com/watch?v=vD2dPFSQ668?autoplay=1&modestbranding=1&frameborder=12"
+                  loop="true"
+                  muted="true"
+                  style={{zIndex:1}}
+                  config={{
+                  youtube: youtubeOptions,
+                }}
+                />
+              </div>
+              
+            </div>
+              <div style={{paddingRight:"150px"}}>
+                <ul>
+                  <li>
+                  Programmed a shell in C similar to the BASH shell, implementing parent/child process handling 
+                  with the Unix process API. The shell can be ran in interactive mode, repeatedly prompting the 
+                  user for input, or be fed commands from a file in non-interactive mode.
+                  </li>
+                  <li>
+                  Features implemented include forking of child processes to allow for commands fed to the shell 
+                  to be ran in its own address space, execution of processes in the background with the "&" command, 
+                  and use of the exec() family of functions to allow the shell to execute non-built in commands 
+                  (searching for the command via the env path variable - https://linux.die.net/man/3/execvp).
+                  </li>
+                  <li>
+                  The shell repeatedly checks for background processes within the same process group and reports if any 
+                  have exited along with their PID. The SIGCONT signal is sent to any stopped child background processes
+                  by the shell to resume their execution. https://linux.die.net/man/2/waitpid
+                  </li>
+                  <li>
+                  Parameter expansion - special shell parameters include $$ for the smallsh process ID GEDPID(3), $? for
+                   the exit status of the last foreground process, and $! for the most recent background process. 
+                   Occurrences of ${} allow for variable expansion for a named environment variable.
+                  </li>
+                  <li>
+                  Implemented custom behavior for SIGINT and SIGSTP signals, allowing the shell to ignore these signals 
+                  while allowing child processes to respond via custom signal handlers and sigaction structure.
+                  </li>
+                  <li>
+                  Parsed command line input into semantic tokens via word splitting. Implemented redirection operators '&lt;', '&gt;', and '&gt;&gt;' to 
+                  redirect STDIN and STDOUT to specific files.
+                  </li>
+                  </ul>
+              </div>
+            </div>
+
+        </section>   
+
     </>
   );
 }
