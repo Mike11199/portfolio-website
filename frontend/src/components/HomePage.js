d2a00cd9f51751c75d7bb774c7439c4f410d612f
Michael Iwanek <miwanek11199@gmail.com>
2023-12-09 23:33:04 -0800

    add solar system project from cs 450 - no github link as private repo

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -52,7 +52,12 @@ import unreal_treasure from '../images/unreal_treasure.png'
 import unreal_voronoi_uniform_fracturing from '../images/unreal_voronoi_uniform_fracturing.png'
 import unreal_patrol from '../images/unreal_patrol.png'
 import unreal_sensing from '../images/unreal_sensing.png'
-
+import SolarSystemFarOut from '../images/solar_system_far_out.gif'
+import EarthClouds from '../images/earth_clouds.gif'
+import Planets1 from '../images/planets_1.png'
+import Planets2 from '../images/planets_2.png'
+import EarthImage from '../images/earth.png'
+import SolarSystemFarOut2 from '../images/solar_system_far_out_2.png'
 
 
 const HomePage = () => {
@@ -742,6 +747,62 @@ const HomePage = () => {
 
           </div>
         </section>
+
+
+     {/* SECTION FOR PROJECT #7 */}
+     <section id="kotlin_section">
+            <h2 className='project_header'>OpenGL C++ Solar System Model</h2>
+
+
+          {/* Buttons */}
+          <div className="project_buttons">
+            <button className="video_button" onClick={() => handleClick("https://www.youtube.com/watch?v=ZvKFpJYDZkw")}>
+              Video Link
+            </button>
+          </div>
+
+            <div className="project_card_hash_map">
+
+              <div className='hash_map_carousel'>
+                <Carousel width="100%" infiniteLoop="true" dynamicHeight="true" showArrows="true">
+                <div><img alt="hash_map_1" src={Planets2} /></div>
+                <div><img alt="hash_map_1" src={EarthClouds } /></div>
+                <div><img alt="hash_map_1" src={SolarSystemFarOut } /></div>
+                <div><img alt="hash_map_1" src={Planets1} /></div>
+                <div><img alt="hash_map_1" src={EarthImage} /></div>
+                <div><img alt="hash_map_1" src={SolarSystemFarOut2} /></div>
+
+                </Carousel>
+              </div>
+
+
+              <div className='hash_map_text'>
+                <ul>
+                  <li>
+                  A Solar System model coded in C++ in OpenGL, using GLSL fragment and vertex shaders, 8k BMP planet textures, and lighting.
+                  Code cannot be posted as this is the project for CS 450 Computer Graphics at Oregon State University, and is a popular final project choice.
+                  </li>
+                  <li>
+                  The vertex and fragment shaders used for rendering planet textures are efficient programs
+                  which run directly on the GPU.  The vertex shader process individual vertices and outputs the transformed position, while the fragment shader outputs the final RGB color of each pixel.
+                  </li>
+                  <li>
+                  Loaded sphere objects for planets and other objects into OpenGL display lists, which are a stored set of commands that can be executed later with a single function call,
+                  reducing the overhead of repeatedly sending commands to the GPU. Added 150 random asteroids to main asteroid belt and another 150 for Kuiper Belt, 8 planets, Pluto, 4 moons, and a galaxy skybox with vertex and fragment shaders.
+                  </li>
+                  <li>
+                  Added clouds to earth by blending two vec4 textures (rgba) textures together in the fragment shader. Used a vec2 variable to translate the clouds over the Earth's surface using a sinusoidal function applied to the program's elapsed time.
+                  </li>
+                  <li>
+                  Used Kepler's third law to calculate realistic orbital periods for planets based on their orbital radii. This tells us that the square of a planet's orbital period is proportional to the cube of
+                  the length of the semi-major axis of its orbit.
+                  </li>
+                  </ul>
+              </div>
+          </div>
+        </section>
+
+
 
 
 
