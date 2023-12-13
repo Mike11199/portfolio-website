0a62aa5fe3f504e09d8e1761e477334ad604b78c
Michael Iwanek <miwanek11199@gmail.com>
2023-12-12 21:06:05 -0800

    clean up opengl page

--- frontend/src/components/Projects/OpenGLSolarSystem.js
+++ frontend/src/components/Projects/OpenGLSolarSystem.js
@@ -1,5 +1,5 @@
-import Planets1 from "../../images/planets_1.png";
-import Planets2 from "../../images/planets_2.png";
+import PlanetsAlignedImage from "../../images/PlanetsAlignedImage.png";
+import AsteroidBeltPlanetsCloseUpPhoto from "../../images/planets_2.png";
 import EarthImage from "../../images/earth.png";
 import SolarSystemFarOut2 from "../../images/solar_system_far_out_2.png";
 
@@ -39,30 +39,30 @@ const OpenGLSolarSystem = () => {
               showArrows="true"
             >
               <div>
-                <img alt="hash_map_1" src={Planets2} />
+                <img alt="image_of_solar_system_jupiter_and_main_asteroid_belt" src={AsteroidBeltPlanetsCloseUpPhoto} />
               </div>
               <div>
                 <img
-                  alt="hash_map_1"
+                  alt="gif_of_earth_and_moon_in_front_of_sun"
                   src="https://github.com/Mike11199/GIFs/raw/main/EarthClouds_%20new.gif"
                 />
               </div>
               <div>
                 <img
-                  alt="hash_map_1"
+                  alt="image_of_whole_solar_system_and_both_belts_from_distance"
                   src={
                     "https://github.com/Mike11199/CS-450-Computer-Graphics-GIFs/assets/91037796/65cbd665-51e0-4e54-9e95-4cfef91b79a0"
                   }
                 />
               </div>
               <div>
-                <img alt="hash_map_1" src={Planets1} />
+                <img alt="close_up_of_planets_aligned_while_program_paused" src={PlanetsAlignedImage} />
               </div>
               <div>
-                <img alt="hash_map_1" src={EarthImage} />
+                <img alt="image_of_moon_and_earth_orbit_static_in_front_of_sun" src={EarthImage} />
               </div>
               <div>
-                <img alt="hash_map_1" src={SolarSystemFarOut2} />
+                <img alt="image_of_solar_system_no_asteroid_belts_no_orbit_lines" src={SolarSystemFarOut2} />
               </div>
             </Carousel>
           </div>
@@ -70,33 +70,35 @@ const OpenGLSolarSystem = () => {
           <div className="hash_map_text">
             <ul>
               <li>
-                A Solar System model coded in C++ in OpenGL, using GLSL fragment
-                and vertex shaders, 8k BMP planet textures, and lighting. Code
-                cannot be posted as this is the project for CS 450 Computer
-                Graphics at Oregon State University, and is a popular final
-                project choice.
+                A Solar System model coded in C++ using OpenGL, featuring GLSL
+                fragment & vertex shaders, 8k BMP planet textures, and advanced
+                lighting. Code cannot be posted as this is the final project for
+                CS 450 Computer Graphics at Oregon State University, and is a
+                popular final project choice.
               </li>
               <li>
                 The vertex and fragment shaders used for rendering planet
                 textures are efficient programs which run directly on the GPU.
                 The vertex shader process individual vertices and outputs the
                 transformed position, while the fragment shader outputs the
-                final RGB color of each pixel.
+                final RGB color of each pixel after it has been processed by the
+                rasterizer.
               </li>
               <li>
                 Loaded sphere objects for planets and other objects into OpenGL
                 display lists, which are a stored set of commands that can be
                 executed later with a single function call, reducing the
                 overhead of repeatedly sending commands to the GPU. Added 150
-                random asteroids to main asteroid belt and another 150 for
-                Kuiper Belt, 8 planets, Pluto, 4 moons, and a galaxy skybox with
-                vertex and fragment shaders.
+                random asteroids to the main asteroid belt and another 150 for
+                Kuiper Belt beyond Neptune, added 8 Planets, Pluto, 4 Moons, and
+                a Galaxy Skybox with vertex and fragment shaders.
               </li>
               <li>
-                Added clouds to earth by blending two vec4 textures (rgba)
-                textures together in the fragment shader. Used a vec2 variable
-                to translate the clouds over the Earth's surface using a
-                sinusoidal function applied to the program's elapsed time.
+                Added equirectangular projection cloud textures to Earth by
+                blending two vec4 (rgba) textures together in the fragment
+                shader. Used a vec2 variable to translate the clouds over the
+                Earth's surface using a sinusoidal function applied to the
+                program's elapsed time.
               </li>
               <li>
                 Used Kepler's third law to calculate realistic orbital periods
