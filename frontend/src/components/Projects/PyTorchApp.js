1148d7653295ab087d9d5ddf667054cdf36138df
Michael Iwanek <miwanek11199@gmail.com>
2023-12-31 19:29:28 -0800

    fix pytorch model app description

--- frontend/src/components/Projects/PyTorchApp.js
+++ frontend/src/components/Projects/PyTorchApp.js
@@ -127,7 +127,6 @@ const PyTorchApp = () => {
                   src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704000819/typescript_new_if4it7.png"
                 />
               </div>
-
             </Carousel>
           </div>
 
@@ -143,33 +142,34 @@ const PyTorchApp = () => {
                 The model is deployed to an AWS SageMaker endpoint which allows
                 it to accept HTTP requests and process images according to a
                 custom inference.py script I've written. This script loads an
-                image as a NumPy array into the torch model and returns its
+                image as a NumPy array into the Torch model and returns its
                 predictions as a JSON object.
               </li>
               <li>
                 The model is invoked by an AWS Lambda which directly sends the
-                image as base64 encoded data. The lambda is in turn invoked by
-                an AWS API Gateway which is configured to directly accept binary
-                image data from an Axios post request.
+                image as Base64 encoded data. The Lambda is in turn invoked by
+                an AWS API Gateway, which is configured to directly accept
+                binary image data from an Axios post request.
               </li>
               <li>
-                The express server takes the image as FormData with the help of
-                multer node.js middleware to upload to the AWS API Gateway. On
-                the front end, a user can upload an URL of an image, which is
-                downloaded and coverted to a blob, then appended to the form
-                data. Or the user can upload an image with a drag and drop, or
-                choose file box, using a custom component.
+                The Express.js server takes the image as form data with the help
+                of multer Node.js middleware to upload to the AWS API Gateway.
+                On the front end, a user can upload an URL of an image which is
+                downloaded and converted to a blob, then appended to the form
+                data. An image file can also be uploaded from their
+                computer/phone with a file explorer pop up, or by dragging and
+                dropping the file onto the web page.
               </li>
               <li>
                 After receiving data from the PyTorch model, the front end uses
-                the JavaScript Canvas to plot the boundary boxes and accuracy of
+                the JavaScript Canvas to plot the bounding boxes and accuracy of
                 the models predictions onto the image provided by the user. The
                 model is capable of analyzing multiple objects in an image and
-                can plot each of these boundary boxes on a supplied image. If a
-                user wants to use an URL, they have the option of selecting
-                images I've pre-selected from a drop down, or copying and
-                pasting their own image URL they've found on the internet. This
-                URL should end with .jpg to work correctly.
+                can plot each of these bounding boxes on a supplied image. If an
+                image URL is preferred over an image upload, a user has the
+                option of choosing images I've pre-selected from a drop down, or
+                copying and pasting their own image URL they've found on the
+                internet.
               </li>
             </ul>
           </div>
