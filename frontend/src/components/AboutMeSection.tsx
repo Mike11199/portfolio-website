ed76a4e50edcaee6b7b5b9c7e03369f8d4c9d48c
Michael Iwanek <miwanek11199@gmail.com>
2024-01-09 22:49:46 -0800

    AboutMeSection refactoring.

--- frontend/src/components/AboutMeSection.tsx
+++ frontend/src/components/AboutMeSection.tsx
@@ -21,7 +21,6 @@ const AboutMeSection = () => {
           About Me <span className="wave">👋</span>
         </h1>
         <div className={styles.aboutMeSection}>
-          {/* Rock Climbing Video - Vent5 */}
           <ReactVideoPlayer
             className={styles.videoWrapperRockClimbing}
             width={playerWidthClimbing}
@@ -30,60 +29,67 @@ const AboutMeSection = () => {
               "https://www.youtube.com/shorts/R_NyGXwE6vY?autoplay=1&modestbranding=1"
             }
           />
-
           <div className={styles.aboutMeImagesAndTextWrapper}>
-            {/* Images of Self */}
-            <div className={styles.aboutMeImages}>
-              <div className={styles.aboutMeImagesFirstRow}>
-                <img
-                  src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864634/about_me_snowshoe_tunnel_qtqonc.jpg"
-                  alt="Michael Iwanek Navbar"
-                  className={styles.aboutMeSnowshoeingPhoto}
-                />
-                <img
-                  src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864636/about_me_heavenly_ski_resort_bu4bzj.jpg"
-                  alt="Michael Iwanek Navbar"
-                  className={styles.aboutMeSkiingPhoto}
-                />
-              </div>
-              <div className="aboutMeImagesSecondRow">
-                <img
-                  src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864634/about_me_tent_snow_camping_qvoznf.jpg"
-                  alt="Michael Iwanek Navbar"
-                  className={styles.tentImage}
-                />
-              </div>
-            </div>
-
-            {/* Self Text Intro */}
-            <div className={styles.textWrapperAboutMe}>
-              <p>Hello!</p>
-              <p>
-                I'm Michael Iwanek and this is my portfolio website to showcase
-                projects I've completed for personal learning and development.
-              </p>
-              <p>
-                I'm currently a Computer Science student at Oregon State
-                University in an accelerated post-baccalaureate program. My
-                first degree was in accounting, and after graduation I worked
-                for about three years as a CPA in public accounting and
-                government roles.
-              </p>
-              <p>
-                I first discovered programming by self-teaching myself VBA to
-                automate work tasks, and discovered how much I enjoy working
-                through coding problems and building things.
-              </p>
-              <p>
-                Outside of work and school, I enjoy rock climbing, skiing, and
-                PC games. I'm hoping to eventually climb multi-pitch routes and
-                get into back-country skiing one day.
-              </p>
-            </div>
+            <AboutMeImagesContainer />
+            <AboutMeTextContainer />
           </div>
         </div>
       </section>
     </>
+  );
+};
+
+const AboutMeImagesContainer = () => {
+  return (
+    <div className={styles.aboutMeImages}>
+      <div className={styles.aboutMeImagesFirstRow}>
+        <img
+          src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864634/about_me_snowshoe_tunnel_qtqonc.jpg"
+          alt="Michael Iwanek Navbar"
+          className={styles.aboutMeSnowshoeingPhoto}
+        />
+        <img
+          src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864636/about_me_heavenly_ski_resort_bu4bzj.jpg"
+          alt="Michael Iwanek Navbar"
+          className={styles.aboutMeSkiingPhoto}
+        />
+      </div>
+      <div className="aboutMeImagesSecondRow">
+        <img
+          src="https://res.cloudinary.com/dwgvi9vwb/image/upload/v1704864634/about_me_tent_snow_camping_qvoznf.jpg"
+          alt="Michael Iwanek Navbar"
+          className={styles.tentImage}
+        />
+      </div>
+    </div>
+  );
+};
+
+const AboutMeTextContainer = () => {
+  return (
+    <div className={styles.textWrapperAboutMe}>
+      <p>Hello!</p>
+      <p>
+        I'm Michael Iwanek and this is my portfolio website to showcase projects
+        I've completed for personal learning and development.
+      </p>
+      <p>
+        I'm currently a Computer Science student at Oregon State University in
+        an accelerated post-baccalaureate program. My first degree was in
+        accounting, and after graduation I worked for about three years as a CPA
+        in public accounting and government roles.
+      </p>
+      <p>
+        I first discovered programming by self-teaching myself VBA to automate
+        work tasks, and discovered how much I enjoy working through coding
+        problems and building things.
+      </p>
+      <p>
+        Outside of work and school, I enjoy rock climbing, skiing, and PC games.
+        I'm hoping to eventually climb multi-pitch routes and get into
+        back-country skiing one day.
+      </p>
+    </div>
   );
 };
 
