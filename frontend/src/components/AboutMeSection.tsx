dbf363ae3fe73eb60b03cd66ac9b93e52653da5a
Michael Iwanek <miwanek11199@gmail.com>
2024-05-05 11:46:19 -0700

    shorten about me

--- frontend/src/components/AboutMeSection.tsx
+++ frontend/src/components/AboutMeSection.tsx
@@ -59,15 +59,12 @@ const AboutMeTextContainer = () => {
       <p>Hello!</p>
       <p>
         I'm Michael Iwanek - and this is my portfolio website to showcase
-        projects I've completed for personal learning and development. I've
-        worked on these applications to keep my skills current, further practice
-        what I've learned in my career, and sometimes to make something
-        interesting for fun as well.
+        projects I've completed for personal learning and development.
       </p>
       <p>
         I'm currently employed as a full stack software engineer, and am a
         recent graduate from Oregon State University - where I obtained a B.S in
-        Computer Science through an accelerated post-bacc program. My first
+        Computer Science through an accelerated post-bacc program. My original
         degree was in accounting, and I successfully switched careers after four
         years as a CPA in public accounting and government roles.
       </p>
@@ -79,7 +76,7 @@ const AboutMeTextContainer = () => {
         PostgreSQL, and others as needed for various projects.
       </p>
       <p>
-        Outside of work and school, I enjoy rock climbing, skiing, and PC games.
+        Outside of work and school, I enjoy rock climbing, skiing, and PC/video games.
         I'm hoping to eventually climb multi-pitch routes and get into
         back-country skiing one day.
       </p>
