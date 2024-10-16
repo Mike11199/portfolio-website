51e372e4231cff71a3dc88adb01b96c8b1e37390
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2024-10-15 21:33:57 -0700

    refactor pytorch project for new ec2 link

--- frontend/src/components/projects/PyTorchApp.tsx
+++ frontend/src/components/projects/PyTorchApp.tsx
@@ -13,25 +13,21 @@ const PyTorchApp = () => {
     fasterrcnn_resnet50_fpn_v2 computer vision model. A TypeScript
     website/server can be used to upload images or provide the URL
     of an image to run computer vision analytics on.`,
-    `The model is deployed to an AWS SageMaker endpoint which allows
-    it to accept HTTP requests and process images according to a
-    custom inference.py script I've written. This script loads an
+
+    `The model is deployed to to an AWS EC2 instance, where a Java Spring Boot
+    API handles requests from the front end and sends binary image data to a
+    Flask microservice.  The Flask app runs a custom inference.py script I've
+    written which invokes the model. This script loads an
     image as a NumPy array into the Torch model and returns its
-    predictions as a JSON object. The model was developed and
-    deployed using a Sagemaker/Jupyter Lab Notebook in Python.`,
+    predictions as a JSON object.`,
 
-    `The model is invoked by an AWS Lambda which directly sends the
-    image as Base64 encoded data. The Lambda is in turn invoked by
-    an AWS API Gateway, which is configured to directly accept
-    binary image data from an Axios post request.`,
-
-    `The Express.js server takes the image as form data with the help
-    of multer Node.js middleware to upload to the AWS API Gateway.
-    On the front end, a user can upload an URL of an image which is
-    downloaded and converted to a blob, then appended to the form
+    `The Java Spring Boot API takes multi-part-form data from the front end to
+    route to the Flask microservice. From the UI, a user can upload an URL of an image
+    which is downloaded and converted to a blob, then appended to the form
     data. An image file can also be uploaded from their
     computer/phone with a file explorer pop up, or by dragging and
     dropping the file onto the web page.`,
+
     `After receiving data from the PyTorch model, the front end uses
     the JavaScript Canvas to plot the bounding boxes and accuracy of
     the models predictions onto the image provided by the user. The
@@ -41,6 +37,13 @@ const PyTorchApp = () => {
     option of choosing images I've pre-selected from a drop down, or
     copying and pasting their own image URL they've found on the
     internet.`,
+    `
+    The model was developed using a Sagemaker/Jupyter Lab Notebook in Python.
+    It was first deployed to an AWS SageMaker endpoint and invoked by an AWS Lambda
+    which directly sends the image as Base64 encoded data to the endpoint. The Lambda is in turn invoked
+    by an AWS API Gateway, which is configured to directly accept binary image data from an
+    Axios post request. Going forward this and Heroku will likely be a backup or turned off due to high
+    costs - and only ran on the EC2 version.`,
   ];
 
   const windowWidth = useWindowWidth();
@@ -55,27 +58,27 @@ const PyTorchApp = () => {
           in various programming languages.
         </p>
         <h2 className={styles.projectHeaderText}>
-          PyTorch Image Classification Website - Python/TypeScript
+          PyTorch Image Classification Website - Java Spring Boot/Flask/TypeScript
         </h2>
 
         {/* Buttons */}
         <div className="projectButtons">
           <GitHubButton
             URL={
-              "https://github.com/Mike11199/PyTorch-Image-Classification-TypeScript"
+              "https://github.com/Mike11199/PyTorch-Image-Classification-Java-Flask-TypeScript"
             }
           />
           <ButtonLink
-            URL={"https://www.youtube.com/watch?v=abtdBPFu_yM"}
+            URL={"https://machine-learning-projects.com/"}
             variant={"blue"}
-            buttonText={"Video"}
+            buttonText={"AWS EC2 (Java/Flask)"}
           />
           <ButtonLink
             URL={
               "https://pytorch-image-model-aws-app-727fe8e23222.herokuapp.com/"
             }
             variant={"red"}
-            buttonText={"Live Website"}
+            buttonText={"Backup Heroku (AWS SageMaker) - Old"}
           />
         </div>
 
