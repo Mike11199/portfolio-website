51249fb43a7581fb389ec0456ab96faf2c82a4ed
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2026-09-06 20:53:48 -0700

    organize navbar into folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { useScroll, animated } from "@react-spring/web";
-import resumePdf  from "../misc/CV_Michael Iwanek.pdf";  // 04_12_2025
-import BinaryDigits from "./about-me-section/BinaryDigits";
-import styles from "./styles/Navbar.module.css";
+import resumePdf  from "../../misc/CV_Michael Iwanek.pdf";  // 04_12_2025
+import BinaryDigits from "./BinaryDigits";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const ProfilePhotoWithName = () => {
