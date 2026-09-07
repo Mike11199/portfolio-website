48dc4521340a11f8f4eef2ff36030074352bba91
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2026-09-06 22:49:30 -0700

    centralize theme colors in a theme.css file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./theme.css";
 import { BrowserRouter } from "react-router-dom";
 import HomePage from "./components/home-page/HomePage";
 import Navbar from "./components/navbar/Navbar";
